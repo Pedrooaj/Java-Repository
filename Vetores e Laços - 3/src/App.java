65affee6ffeaa324a46f3937783a64b0581a0e89
Pedrooaj <pedroantonio5735@gmail.com>
2024-09-19 20:38:09 -0300

    Update

--- Vetores e Laços - 3/src/App.java
+++ Vetores e Laços - 3/src/App.java
@@ -1,14 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         Sala sala = new Sala(25);
 
         Aluno a1 = new Aluno("123.312.789-12", 18);
         Aluno a2 = new Aluno("123.121-12", 27);
         Aluno a3 = new Aluno("123.456.213-12", 35);
-        sala.addAluno(a1);
-        sala.addAluno(a2);
-        sala.addAluno(a3);
+        // Adiciona um unico aluno
+        sala.addAlunos(a1);
+        //adiciona varios alunos
+        sala.addAlunos(a2,a3);
         sala.mostrarAlunos();
     }
 }
--- Vetores e Laços - 3/src/Sala.java
+++ Vetores e Laços - 3/src/Sala.java
@@ -7,14 +7,26 @@ public class Sala {
         this.indice = 0;
     }
 
+    
     public void mostrarAlunos(){
         for(int i = 0; i < this.indice; i++){
             alunos[i].mostrarInfo();
         }
     }
 
+
+    // metodo para adicionar um unico aluno 
     public void addAluno(Aluno aluno){
         this.alunos[this.indice] = aluno;
         this.indice++;
     }
+
+    // metodo para adicionar varios alunos
+    public void addAlunos(Aluno... args){
+        for(int i = 0; i < args.length; i++){
+            this.addAluno(args[i]);
+        }
+    }
+
+
 }
